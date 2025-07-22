d0ba3e9348ab2f88cae14f14f879b62e4caa6e03
Klit-Bucinca <klitbucinca@gmail.com>
2025-07-22 13:23:36 +0200

    Added the first model,controller,dbContext

---
+++ backend/SkillHire/Controllers/UserController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SkillHire.Data;
+using SkillHire.Models;
+
+namespace SkillHire.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public UsersController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Users
+        [HttpGet]
+        public IActionResult GetUsers()
+        {
+            return Ok(_context.Users.ToList());
+        }
+
+        // POST: api/Users
+        [HttpPost]
+        public IActionResult CreateUser(User user)
+        {
+            _context.Users.Add(user);
+            _context.SaveChanges();
+            return CreatedAtAction(nameof(GetUsers), new { id = user.Id }, user);
+        }
+    }
+}
---
+++ backend/SkillHire/Data/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SkillHire.Models;
+
+namespace SkillHire.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
