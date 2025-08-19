dad25b3b0e38e0b5c2b210e068759296276c9e86
Klit-Bucinca <klitbucinca@gmail.com>
2025-08-19 14:13:07 +0200

    Made the login page better visually

--- frontend/src/views/auth/Login.js
+++ frontend/src/views/auth/Login.js
@@ -2,6 +2,9 @@ import React, { useState } from "react";
 import { Link, useHistory } from "react-router-dom";
 import axios from "axios";
 
+
+import SkillHireLogo from "assets/img/skillhire-logo2.png"; 
+
 export default function Login() {
   const [formData, setFormData] = useState({ username: "", password: "" });
   const history = useHistory();
@@ -17,19 +20,13 @@ export default function Login() {
       localStorage.setItem("token", res.data.token);
       localStorage.setItem("user", JSON.stringify(res.data));
 
-       const role = res.data.role;
-      
+      const role = res.data.role;
       alert("Login successful!");
 
-      if (role === "Admin") {
-        history.push("/admin/dashboard");
-      } else if (role === "Client") {
-        history.push("/client/dashboard");
-      } else if (role === "Worker") {
-        history.push("/worker/dashboard");
-      } else {
-        history.push("/auth/login");
-      }
+      if (role === "Admin") history.push("/admin/dashboard");
+      else if (role === "Client") history.push("/client/dashboard");
+      else if (role === "Worker") history.push("/worker/dashboard");
+      else history.push("/auth/login");
     } catch (err) {
       alert("Login failed: " + (err.response?.data || err.message));
     }
@@ -37,118 +34,85 @@ export default function Login() {
 
   return (
     <>
-      <div className="container mx-auto px-4 h-full">
-        <div className="flex content-center items-center justify-center h-full">
-          <div className="w-full lg:w-4/12 px-4">
-            <div className="relative flex flex-col min-w-0 break-words w-full mb-6 shadow-lg rounded-lg bg-blueGray-200 border-0">
-              <div className="rounded-t mb-0 px-6 py-6">
-                <div className="text-center mb-3">
-                  <h6 className="text-blueGray-500 text-sm font-bold">
-                    Sign in with
-                  </h6>
-                </div>
-                <div className="btn-wrapper text-center">
-                  <button
-                    className="bg-white active:bg-blueGray-50 text-blueGray-700 font-normal px-4 py-2 rounded outline-none focus:outline-none mr-2 mb-1 uppercase shadow hover:shadow-md inline-flex items-center font-bold text-xs ease-linear transition-all duration-150"
-                    type="button"
-                  >
+      <div
+        className="min-h-screen bg-no-repeat bg-cover bg-center"
+        style={{
+          backgroundImage: `url(${require("assets/img/backgroundf.png")})`,
+        }}
+      >
+        <div className="container mx-auto px-4 h-full">
+          <div className="flex content-center items-center justify-center h-full">
+            <div className="w-full lg:w-4/12 px-4">
+              <div className="relative flex flex-col min-w-0 break-words w-full mb-6 shadow-lg rounded-lg bg-blueGray-200 border-0 mt-20">
+                {/* Card Header */}
+                <div className="rounded-t mb-0 px-6 py-6">
+                  <div className="text-center mb-3">
+                    {}
                     <img
-                      alt="..."
-                      className="w-5 mr-1"
-                      src={require("assets/img/github.svg").default}
+                      src={SkillHireLogo}
+                      alt="SkillHire"
+                      className="h-25 mx-auto"
                     />
-                    Github
-                  </button>
-                  <button
-                    className="bg-white active:bg-blueGray-50 text-blueGray-700 font-normal px-4 py-2 rounded outline-none focus:outline-none mr-1 mb-1 uppercase shadow hover:shadow-md inline-flex items-center font-bold text-xs ease-linear transition-all duration-150"
-                    type="button"
-                  >
-                    <img
-                      alt="..."
-                      className="w-5 mr-1"
-                      src={require("assets/img/google.svg").default}
-                    />
-                    Google
-                  </button>
-                </div>
-                <hr className="mt-6 border-b-1 border-blueGray-300" />
-              </div>
-              <div className="flex-auto px-4 lg:px-10 py-10 pt-0">
-                <div className="text-blueGray-400 text-center mb-3 font-bold">
-                  <small>Or sign in with credentials</small>
-                </div>
-                <form>
-                  <div className="relative w-full mb-3">
-                    <label
-                      className="block uppercase text-blueGray-600 text-xs font-bold mb-2"
-                    >
-                      Email
-                    </label>
-                    <input
-                      type="text"
-                      name="username"
-                      value={formData.username}
-                      onChange={handleChange}
-                      className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
-                      placeholder="Email or Username"
-                    />
-                  </div>
 
-                  <div className="relative w-full mb-3">
-                    <label
-                      className="block uppercase text-blueGray-600 text-xs font-bold mb-2"
-                    >
-                      Password
-                    </label>
-                    <input
-                      type="password"
-                      name="password"
-                      value={formData.password}
-                      onChange={handleChange}
-                      className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
-                      placeholder="Password"
-                    />
                   </div>
+                  <hr className="mt-4 border-b-1 border-blueGray-300" />
+                </div>
 
-                  <div>
-                    <label className="inline-flex items-center cursor-pointer">
+                {/* Card Body */}
+                <div className="flex-auto px-4 lg:px-10 py-10 pt-0">
+                  <div className="text-blueGray-400 text-center mb-3 font-bold">
+                    <small>Sign in with credentials</small>
+                  </div>
+                  <form>
+                    <div className="relative w-full mb-3">
+                      <label className="block uppercase text-blueGray-600 text-xs font-bold mb-2">
+                        Email
+                      </label>
                       <input
-                        id="customCheckLogin"
-                        type="checkbox"
-                        className="form-checkbox border-0 rounded text-blueGray-700 ml-1 w-5 h-5 ease-linear transition-all duration-150"
+                        type="text"
+                        name="username"
+                        value={formData.username}
+                        onChange={handleChange}
+                        className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
+                        placeholder="Email or Username"
                       />
-                      <span className="ml-2 text-sm font-semibold text-blueGray-600">
-                        Remember me
-                      </span>
-                    </label>
-                  </div>
+                    </div>
 
-                  <div className="text-center mt-6">
-                    <button
-                      className="bg-blueGray-800 text-white active:bg-blueGray-600 text-sm font-bold uppercase px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 w-full ease-linear transition-all duration-150"
-                      type="button"
-                      onClick={handleLogin}
-                    >
-                      Sign In
-                    </button>
-                  </div>
-                </form>
+                    <div className="relative w-full mb-3">
+                      <label className="block uppercase text-blueGray-600 text-xs font-bold mb-2">
+                        Password
+                      </label>
+                      <input
+                        type="password"
+                        name="password"
+                        value={formData.password}
+                        onChange={handleChange}
+                        className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
+                        placeholder="Password"
+                      />
+                    </div>
+
+                    <div className="text-center mt-6">
+                      <button
+                        className="bg-blueGray-800 text-white active:bg-blueGray-600 text-sm font-bold uppercase px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 w-full ease-linear transition-all duration-150"
+                        type="button"
+                        onClick={handleLogin}
+                      >
+                        Sign In
+                      </button>
+                    </div>
+                  </form>
+                </div>
               </div>
-            </div>
-            <div className="flex flex-wrap mt-6 relative">
-              <div className="w-1/2">
-                <a
-                  href="#pablo"
-                  onClick={(e) => e.preventDefault()}
-                  className="text-blueGray-200"
-                >
-                  <small>Forgot password?</small>
-                </a>
-              </div>
-              <div className="w-1/2 text-right">
-                <Link to="/auth/register" className="text-blueGray-200">
-                  <small>Create new account</small>
-                </Link>
+
+              {/* Footer */}
+              <div className="flex flex-wrap mt-6 relative">
+                <div className="w-1/2"></div>
+                <div className="w-1/2 text-right">
+                  <Link to="/auth/register" className="text-blueGray-200">
+                    <small>Create new account</small>
+                  </Link>
+                </div>
               </div>
             </div>
           </div>
